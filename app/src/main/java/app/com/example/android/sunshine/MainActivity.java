dbf4c4330c5db53a96074156af03d97e69ea3f0c
Uriel Carrillo <uriel.carrillo@caffenio.com>
2016-01-08 16:09:35 -0700

    Added a new item on the menu for maps, it start an intent that shows the map of the location in shared preferences

--- app/src/main/java/app/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/android/sunshine/MainActivity.java
@@ -2,6 +2,9 @@ package app.com.example.android.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,13 +33,23 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Context context = getApplicationContext();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Context context = getApplicationContext();
+
             Intent intent = new Intent(context,SettingsActivity.class);
             startActivity(intent);
             return true;
+        }
+        if(id==R.id.action_map){
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            String location = prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+
+            String map = "http://maps.google.co.in/maps?q=" + location;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
         }
 
         return super.onOptionsItemSelected(item);
